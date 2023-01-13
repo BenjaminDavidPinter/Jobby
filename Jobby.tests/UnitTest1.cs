7197ad7c2fe73a536c830444fbca67a1fc40bc6a
Benjamin Pinter <72715672+BenjaminDavidPinter@users.noreply.github.com>
2023-01-13 08:40:32 -0500

    Clean project structure

---
+++ Jobby.tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Jobby.tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Jobby.worker/Program.cs
@@ -0,0 +1,10 @@
+using Jobby.worker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
